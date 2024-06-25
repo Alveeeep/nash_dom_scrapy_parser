14fd76cc3f48ab1cd18be598cfb0b33125d8f7a3
Alveeeep <alvir.misalimov@gmail.com>
2024-06-25 17:38:34 +0300

    ready

--- testnashdom/testnashdom/spiders/parse.py
+++ testnashdom/testnashdom/spiders/parse.py
@@ -27,7 +27,7 @@ class ParseSpider(scrapy.Spider):
     start_urls = [
         'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%B5%D0%BA/%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2/%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA?place=0-1&objStatus=0']
     offset = 0
-    total = 792
+    total = 0
 
     def parse(self, response):
         api_url = self.base_url + self.api % 0
@@ -37,10 +37,10 @@ class ParseSpider(scrapy.Spider):
     def parse_api(self, response):
         url = 'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/object/'
         data = json.loads(response.text)
-        print(data)
+        data = data.get(data)
+        self.total = data.get('total')
         for el in data.get('list', []):
             id = el.get('objId')
-            print(id)
             yield scrapy.Request(url=url + str(id), method='GET', headers=self.headers, callback=self.parse_page,
                                  meta={"impersonate": 'chrome110'})
         if self.offset < self.total:
@@ -50,6 +50,7 @@ class ParseSpider(scrapy.Spider):
 
     def parse_page(self, response):
         data = json.loads(response.text)
+        data = data.get('data')
         res = {}
         res['name'] = data.get('nameObj')
         res['address'] = data.get('address')
